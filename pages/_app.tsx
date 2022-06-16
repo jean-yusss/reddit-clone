adad7dec9211899b6772253ae258848aea34bf3e
Patrick Nguyen <iamtiredofyou2@gmail.com>
2022-06-16 18:35:49 -0400

    Setup database

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
+import { ApolloProvider } from '@apollo/client';
 import { SessionProvider } from 'next-auth/react';
 
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 
+import client from '../apollo-client';
+
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
-	<SessionProvider session={session}>
-		<div className='h-screen overflow-y-scroll bg-slate-200'>
-			<Header />
-			<Component {...pageProps} />
-		</div>
-	</SessionProvider>
+	<ApolloProvider client={client}>
+		<SessionProvider session={session}>
+			<div className='h-screen overflow-y-scroll bg-slate-200'>
+				<Header />
+				<Component {...pageProps} />
+			</div>
+		</SessionProvider>
+	</ApolloProvider>
 );
 
 export default App;

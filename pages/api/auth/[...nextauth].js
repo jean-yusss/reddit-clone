fc493f9d0b8228c14dc9fcf96623a94882b291d3
Patrick Nguyen <iamtiredofyou2@gmail.com>
2022-06-25 20:52:30 -0400

    Implemented sign in with google functionality

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import RedditProvider from 'next-auth/providers/reddit';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
 	// Configure one or more authentication providers
@@ -7,7 +8,11 @@ export default NextAuth({
 		RedditProvider({
 			clientId: process.env.REDDIT_CLIENT_ID,
 			clientSecret: process.env.REDDIT_CLIENT_SECRET
-		})
+		}),
 		// ...add more providers here
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET
+		})
 	]
 });
